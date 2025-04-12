88e193b393dc1218ca0d8a85bea6b0342701ee2b
TavaresBaker <tavaresbaker15@gmail.com>
2025-04-12 13:25:12 -0400

    Update Restore.sh

--- Restore.sh
+++ Restore.sh
@@ -14,15 +14,15 @@ echo "[+] Backup complete at /root/backups_php/www"
 # Step 4: Go to /usr/local
 cd /usr/local || exit 1
 
-# Step 5: Download fresh www directory from GitHub (RELENG_2_7_0)
-fetch https://codeload.github.com/pfsense/pfsense/zip/refs/heads/RELENG_2_7_0 -o pfsense.zip
+# Step 5: Download fresh www directory from GitHub (RELENG_2_7_2)
+fetch https://codeload.github.com/pfsense/pfsense/zip/refs/heads/RELENG_2_7_2 -o pfsense.zip
 
 # Step 6: Unzip and replace www
 unzip -oq pfsense.zip
 rm -rf www
-cp -a pfsense-RELENG_2_7_0/src/usr/local/www .
+cp -a pfsense-RELENG_2_7_2/src/usr/local/www .
 
 # Step 7: Cleanup
-rm -rf pfsense.zip pfsense-RELENG_2_7_0
+rm -rf pfsense.zip pfsense-RELENG_2_7_2
 
 echo "[✔] /usr/local/www has been replaced from GitHub."
